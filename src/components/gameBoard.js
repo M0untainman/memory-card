f935fa6ed5934a43ee313dd22a3f6c6559b29f40
samuel <samuel.chowles@gmail.com>
2022-01-12 17:33:49 +0200

    can register clicks on  individual divs

--- src/components/gameBoard.js
+++ src/components/gameBoard.js
@@ -1,18 +1,4 @@
-import React from 'react';
-let cards = [
-  'card 1',
-  'card 2',
-  'card 3',
-  'card 4',
-  'card 5',
-  'card 6',
-  'card 7',
-  'card 8',
-  'card 9',
-  'card 10',
-  'card 11',
-  'card 12',
-];
+import React, { useState, useEffect } from 'react';
 
 //shuffle algorithim found online. search Fisher-yates shuffle
 const shuffle = (array) => {
@@ -20,7 +6,7 @@ const shuffle = (array) => {
     randomIndex;
 
   // While there remain elements to shuffle...
-  while (currentIndex != 0) {
+  while (currentIndex !== 0) {
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
@@ -35,11 +21,48 @@ const shuffle = (array) => {
   return array;
 };
 
+let cards = [
+  { key: 1, name: 'card 1' },
+  { key: 2, name: 'card 2' },
+  { key: 3, name: 'card 3' },
+  { key: 4, name: 'card 4' },
+  { key: 5, name: 'card 5' },
+  { key: 6, name: 'card 6' },
+  { key: 7, name: 'card 7' },
+  { key: 8, name: 'card 8' },
+  { key: 9, name: 'card 9' },
+  { key: 10, name: 'card 10' },
+  { key: 11, name: 'card 11' },
+  { key: 12, name: 'card 12' },
+];
+
+//card component
+const Card = (props) => {
+  const [clicked, setClicked] = useState(false);
+  const changeClicked = () => {
+    if (clicked === false) {
+      setClicked(true);
+    } else {
+      console.log('already clicked');
+    }
+  };
+  const sayhi = () => {
+    alert('works');
+  };
+
+  return (
+    <div onClick={changeClicked} className='card'>
+      {props.whichCard}
+    </div>
+  );
+};
+
+// Gameboard component
 const Gameboard = () => {
   return (
     <div className='cardHolder'>
       {shuffle(cards).map((card) => {
-        return <div className='card'>{card}</div>;
+        return <Card key={card.key} whichCard={card.name}></Card>;
       })}
     </div>
   );
