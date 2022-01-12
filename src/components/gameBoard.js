ebb616c68e3627cddc6da4fb8affa3d344533eb8
samuel <samuel.chowles@gmail.com>
2022-01-12 11:01:12 +0200

    cards render in random order

--- src/components/gameBoard.js
+++ src/components/gameBoard.js
@@ -1,23 +1,44 @@
 import React from 'react';
 let cards = [
-  'card1',
-  'card2',
-  'card3',
-  'card4',
-  'card5',
-  'card6',
-  'card7',
-  'card8',
-  'card9',
-  'card10',
-  'card11',
-  'card12',
+  'card 1',
+  'card 2',
+  'card 3',
+  'card 4',
+  'card 5',
+  'card 6',
+  'card 7',
+  'card 8',
+  'card 9',
+  'card 10',
+  'card 11',
+  'card 12',
 ];
+
+//shuffle algorithim found online. search Fisher-yates shuffle
+const shuffle = (array) => {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
 
 const Gameboard = () => {
   return (
     <div className='cardHolder'>
-      {cards.map((card) => {
+      {shuffle(cards).map((card) => {
         return <div className='card'>{card}</div>;
       })}
     </div>
